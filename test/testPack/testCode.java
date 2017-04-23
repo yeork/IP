b3189bcd4dafef0be669c846ce28fcf1fa199df6
yeork <naomy.valdez@live.fr>
2017-04-22 21:02:19 -0400

    Add files via upload

---
+++ test/testPack/testCode.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testPack;
+
+/**
+ *
+ * @author Jeremiah
+ */
+public class testCode {
+    
+    //used in 
+            /*
+            if (event.getSource() == UserInterface.startBtn) {
+                runningAnimation = true;
+                Functions.timeline.play();
+                Functions.btnStatus(runningAnimation);  HERE
+                }
+            if (event.getSource() == UserInterface.pauseBtn) {
+                runningAnimation = false;
+                Functions.timeline.pause();
+                Functions.btnStatus(runningAnimation);  HERE
+            }
+            */
+    
+    //HERE IS METHOD which was in functions
+    
+     /*
+    public static void btnStatus(boolean status) {
+        UserInterface.startBtn.setDisable(status);
+        UserInterface.pauseBtn.setDisable(!status);
+        UserInterface.resetBtn.setDisable(!status);
+        switch (IntegrativeMain.x) {
+            case 1: // Newton's Second Law
+                break;
+            case 2: // Kinetic Energy
+                break;
+            case 3: // SHM
+                UserInterface.positionSlider.setDisable(status);
+                UserInterface.angVelSlider.setDisable(status);
+                break;
+            case 4: // Doppler
+                UserInterface.speedCarSlider.setDisable(status);
+                UserInterface.freqSlider.setDisable(status);
+                break;
+            case 5: // Coulomb's Law
+                UserInterface.switchSignsBtn.setDisable(status);
+                break;
+            case 6: // Electric Potential
+                break;
+        }
+    }*/
+    
+    //UNDER HANDLER IN IntegrativeMain
+    /*
+            if (event.getSource() == UserInterface.distanceSlider){
+                Functions.setAnimGraph();
+            }*/
+    
+}
